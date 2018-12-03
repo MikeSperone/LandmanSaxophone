4c1a9d9a9c271f7b54b3498d7cccfd7f423109f9
Michael Sperone (landman api) <mike@mikesperone.com>
2018-12-03 18:12:31 +0000

    fix body-parser warning

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors({
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
